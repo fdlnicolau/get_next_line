cecf1ad85b22f04dda2de7f7eebc6751b63305cb
fdlnicolau <149716240+fdlnicolau@users.noreply.github.com>
2024-02-08 12:55:50 +0100

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,93 +10,95 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_free(char **str)
+#include "get_next_line.h"
+
+char	*duplicate_string(char *s1)
 {
-	free(*str);//libera
-	*str = NULL;//establece
-	return (NULL);//indica
-}
-char ft_read (int fd, char *statica)
-{
-	int numbytes;
-	char *buffer;
+	char			*new_chain;
+	unsigned int	i;
 
-	numbytes = 1;
-
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));//sizeof mira lo que ocupa char
-	if (!buffer)
-		return (ft_free(&statica));
-		
-	buffer[0] = '\0';//la reinicio
-
-	while (numbytes > 0 && !strchr(buffer '\n'))
-	{
-		numbytes = read (fd, buffer, BUFFER_SIZE);
-		if (numbytes > 0)
-		{
-				buffer[numbytes] = '\0';
-				statica = ft_strjoing_get(buffer, statica);	
-		}
-	}
-
-	free(buffer);//Liberamos el buffer porque ya no lo necesitamos
-
-	if (numbytes  == -1)
-		return(free(&statica));//en caso de que READ falle liberamos la estatica
-		
-	return(statica);
-}
-
-char *ft_get_line(char estatic)//coje la caddena statoc y la mete en line hasta el salto de linea
-{
-	char	*cadenatica;//contiene lo que tiene static
-	char	*up;//apunta al salto de linea
-	int		len;
-
-	up = ft_strchr(static, '\n');//bucacmos el salto de linea
-	len = (up - static) + 1;
-	cadenatica = ft_subster(static, 0, len);//creamos nueva cadena con el contenido del inicio hasta len
-
-	if(!cadenatica);
-		return(NULL);
-
-	return (cadenatica);
-}
-
-char *ft_new_static(char *estatic)
-{
-	char	*rest_estatic;
-	char	*up;
-	int		len;
-
-	up = ft_strchr(estatic, '\n');//bucacmos el salto de linea
-	if (!up)
-	{
-		*rest_static = NULL;
-		return (ft_free(&estatic));
-	}
-	else
-		len = (up - reset_static) + 1;
-	if (!estatic[len])
-		return (ft_free(&estatic));
-	
-	//metemos lo restante en reset_estatic
-	reset_estatic = ft_substr(estatic, len, ft_strlen(estatic) - len);
-	ft_free(&estatic);//liberamos la antigua
-
-	if (!new_estatic)
+	if (!s1)
 		return (NULL);
-	return (rest_static);
+
+	i = -1;
+	new_chain = (char *)ft_calloc((ft_strlen(s1) + 1), sizeof(char));
+	if (!new_chain)
+		return (NULL);
+
+	while (s1[++i])
+		new_chain[i] = s1[i];
+
+	return (new_chain);
 }
+
+char	*update_and_extract_next_line(char *static_s)
+{
+	char	*after_line_break;
+	
+	after_line_break = duplicate_string(ft_strchr(static_s, '\n'));
+
+	free(static_s);
+
+	if (after_line_break)
+		static_s = duplicate_string(after_line_break + 1);
+	else
+		static_s = NULL;
+	
+	return (free(after_line_break), static_s);
+}
+
+char	*reader(char *static_s, int fd)
+{
+	int		bytes_read;
+	char	*buffer;
+
+	bytes_read = 1;
+
+	buffer = ft_calloc(BUFFER_SIZE + 1, 1);
+
+	if (!buffer)
+		return (NULL);
+	
+	while (!ft_strchr(static_s, '\n') && bytes_read > 0)
+	{
+		bytes_read =read(fd, buffer, BUFFER_SIZE);
+
+		if (bytes_read < 0)
+			return (free(buffer), free(static_s), NULL);
+
+		if (!static_s)
+			static_s = ft_calloc(1, 1);
+
+		static_s = gnl_strjoin(static_s, buffer);
+
+		if (!*static_s)
+			return (free(static_s), free(buffer), buffer = NULL, NULL);
+	}
+	return (free(buffer), static_s);
+}
+
 char *get_next_line(int fd)
 {
-	static char*	statica[18937];
-	char line;
+	static char		*static_s;
+	char			*line;
+	int				i;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return(ft_free(&static));
-		
-	ft_read (fd, statica);
-	if(!statica)
+	i = 0;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
+	
+	static_s = reader(static_s, fd);
+
+	if (!static_s)
+		return(NULL);
+	
+	while (static_s[i] != '\n' && static_s[i])
+		i++;
+	
+	line = ft_substr(static_s, 0, i + 1);
+
+	static_s = update_and_extract_next_line(static_s);
+
+	return(line);
 }
